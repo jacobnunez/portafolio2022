bc5593a5545d9ac65a5cf437be34eae848c05ecc
Maximiliano <max.tapiam@alumnos.duoc.cl>
2022-04-20 00:02:19 -0400

    act-prueba

--- src/app/components/administrador/administrador.module.ts
+++ src/app/components/administrador/administrador.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { AdministradorComponent } from './administrador.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+//Prueba 
 
 @NgModule({
   declarations: [
